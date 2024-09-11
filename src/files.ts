0ed14f70af9e3545ac5ab21043eacba5939c9377
Brandon <brandon@webtender.host>
2024-09-12 10:57:15 +1200

    feat: file uploads

--- src/files.ts
+++ src/files.ts
@@ -1,4 +1,5 @@
 import DatanestClient, { PaginatedResponse, Timestamp, UUID } from "./index";
+import type { File as NodeFile } from "node:buffer";
 
 export enum VirusStatus {
     /**
@@ -46,10 +47,14 @@ export type File = {
     size_mb: null | number;
     version: number;
     is_final: boolean;
+    /** The file record is awaiting an upload. The file will have an `expires_at` property if upload is not completed in time. */
+    is_pending: boolean;
     virus_status: VirusStatus;
     review_status: ReviewStatus;
     review_comments: string | null;
     formatter_comments: string | null;
+    /** Used with `is_pending` */
+    expires_at: Timestamp | null;
     created_at: Timestamp;
     updated_at: Timestamp;
     deleted_at: Timestamp | null;
@@ -87,4 +92,83 @@ export async function getProjectFile(client: DatanestClient, projectUuid: UUID, 
          */
         temporary_url?: string
     };
+}
+
+export async function uploadFile(client: DatanestClient, projectUuid: UUID, path: string, name: string, file: NodeFile | Blob | Buffer | string, options?: { create_notification?: boolean; }): Promise<File> {
+    const uploadUrl = await createNewFileUploadUrl(client, projectUuid, path, name, options);
+
+    const response = await fetch(uploadUrl.upload_put_url, {
+        method: 'PUT',
+        headers: uploadUrl.upload_put_headers,
+        body: file,
+    } as RequestInit);
+    if (!response.ok) {
+        throw new Error('Failed to upload file: ' + response.status + ' ' + await response.text());
+    }
+
+    return await acceptFile(client, projectUuid, uploadUrl.uuid);
+}
+
+export async function createNewFileUploadUrl(client: DatanestClient, projectUuid: UUID, path: string, name: string, options?: { create_notification?: boolean; }) {
+    const response = await client.post('v1/projects/' + projectUuid + '/files/upload-url', {
+        path,
+        name,
+        create_notification: options?.create_notification,
+    });
+
+    const data = await response.json();
+    return data as File & {
+        uuid: string;
+        /**
+         * Temporary URL to upload the file
+         * Make a PUT request with the `upload_headers`
+         */
+        upload_put_url: string;
+        upload_put_headers: Record<string, string>;
+        accept_post_url: string;
+    };
+}
+
+export async function acceptFile(client: DatanestClient, projectUuid: UUID, fileUuid: UUID): Promise<File> {
+    const response = await client.post('v1/projects/' + projectUuid + '/files/' + fileUuid + '/accept-upload');
+    const data = await response.json();
+    return data as File;
+}
+
+export async function deleteFile(client: DatanestClient, projectUuid: UUID, fileUuid: UUID) {
+    await client.delete('v1/projects/' + projectUuid + '/files/' + fileUuid);
+}
+
+export async function getRecentNotifications(client: DatanestClient, projectUuid: UUID, page: number = 1) {
+    const response = await client.get('v1/projects/' + projectUuid + '/notifications', { page });
+    const data = await response.json();
+    return data as PaginatedResponse<{
+        id: number;
+        project_uuid: UUID;
+        /** Typically available when status = completed */
+        file_uuid: UUID | null;
+        type: 'plan'
+        | 'parsing'
+        | 'xlsx'
+        | 'docx'
+        | 'zip'
+        | 'shapefile'
+        | 'photolog'
+        | 'image'
+        | 'custom-document'
+        | 'gather-table'
+        | 'proucl-table'
+        | 'other'
+        | 'ai-document-review'
+        | 'leaderboard';
+        status: 'queued'
+        | 'in_progress'
+        | 'completed'
+        | 'failed'
+        | 'converting';
+        /** When `file_id` is not null, otherwise file is not defined. */
+        file?: File;
+        created_at: Timestamp;
+        updated_at: Timestamp;
+    }>;
 }
--- tests/files-integration.spec.ts
+++ tests/files-integration.spec.ts
@@ -1,12 +1,13 @@
 import { it, expect } from 'vitest';
 import dotenv from 'dotenv';
 import DatanestClient from '../src';
-import { getProjectFile, getProjectFiles } from '../src/files';
+import { deleteFile, getProjectFile, getProjectFiles, getRecentNotifications, uploadFile } from '../src/files';
+import { readFileSync, unlink, unlinkSync, writeFileSync } from 'fs';
 
 dotenv.config();
 
 if (process.env.DATANEST_API_KEY && process.env.DATANEST_API_SECRET && process.env.DATANEST_API_BASE_URL) {
-    it('GET project files, if there is a file check you can get the project link', async () => {
+    it.concurrent('GET project files, if there is a file check you can get the project link', async () => {
         const client = new DatanestClient();
         const fileProjectUuid = process.env.FILE_PROJECT_UUID || process.env.ENVIRO_PROJECT_UUID || 'd91c8a4e-5dc8-48ba-bdc1-5584ff94b4c9';
         const files = await getProjectFiles(client, fileProjectUuid);
@@ -31,6 +32,68 @@ if (process.env.DATANEST_API_KEY && process.env.DATANEST_API_SECRET && process.e
         expect(file.is_final).is.a('boolean');
         expect(file.virus_status).is.a('number');
         expect(file.review_status).is.a('number');
+    });
+
+    it.concurrent('Can upload a blob to a project', async () => {
+        const fileProjectUuid = process.env.FILE_PROJECT_UUID || process.env.ENVIRO_PROJECT_UUID || 'd91c8a4e-5dc8-48ba-bdc1-5584ff94b4c9';
+        const client = new DatanestClient();
+
+        const file = await uploadFile(client, fileProjectUuid, 'API Upload', 'test.txt', new Blob(['Hello, World!']));
+        expect(file.uuid).is.a('string');
+        expect(file.display_name).toBe('test.txt');
+        expect(file.path).toBe('API Upload');
+        expect(file.size_mb).toBeLessThan(0.01);
+
+        const downloadedContents = await (await fetch(file.temporary_url!)).text();
+        expect(downloadedContents).toBe('Hello, World!');
+
+        // Cleanup
+        await deleteFile(client, fileProjectUuid, file.uuid)
+    });
+
+    it.concurrent('Can upload a local file with fs readFileSync api and create a export download notification', async () => {
+        const fileProjectUuid = process.env.FILE_PROJECT_UUID || process.env.ENVIRO_PROJECT_UUID || 'd91c8a4e-5dc8-48ba-bdc1-5584ff94b4c9';
+        const client = new DatanestClient();
+
+        writeFileSync('./test-with-notification.txt', 'Hello, World 2');
+        const file = await uploadFile(client, fileProjectUuid, 'API Upload', 'test-with-notification.txt', readFileSync('./test-with-notification.txt'), { create_notification: true });
+        unlinkSync('./test-with-notification.txt');
+        expect(file.uuid).is.a('string');
+        expect(file.display_name).toBe('test-with-notification.txt');
+        expect(file.path).toBe('API Upload');
+        expect(file.size_mb).toBeLessThan(0.01);
+
+        const notifications = await getRecentNotifications(client, file.project_uuid);
+        const fileNotification = notifications.data.find(n => n.file_uuid === file.uuid);
+        expect(fileNotification).is.an('object');
+        expect(fileNotification!.file).is.an('object');
+        expect(fileNotification!.file!.uuid).toBe(file.uuid);
+        expect(fileNotification!.file!.display_name).toBe('test-with-notification.txt');
+
+        const downloadedContents = await (await fetch(file.temporary_url!)).text();
+        expect(downloadedContents).toBe('Hello, World 2');
+
+        await deleteFile(client, fileProjectUuid, file.uuid);
+
+        const notifications2 = await getRecentNotifications(client, file.project_uuid);
+        const fileNotification2 = notifications2.data.find(n => n.file_uuid === file.uuid);
+        expect(fileNotification2).toBe(undefined);
+    });
+
+    it.concurrent('Can upload a string as a file', async () => {
+        const fileProjectUuid = process.env.FILE_PROJECT_UUID || process.env.ENVIRO_PROJECT_UUID || 'd91c8a4e-5dc8-48ba-bdc1-5584ff94b4c9';
+        const client = new DatanestClient();
+
+        const file = await uploadFile(client, fileProjectUuid, 'API Upload', 'test-string-upload.txt', 'My content as a string', { create_notification: true });
+        expect(file.uuid).is.a('string');
+        expect(file.display_name).toBe('test-string-upload.txt');
+        expect(file.path).toBe('API Upload');
+        expect(file.size_mb).toBeLessThan(0.01);
+
+        const downloadedContents = await (await fetch(file.temporary_url!)).text();
+        expect(downloadedContents).toBe('My content as a string');
+
+        await deleteFile(client, fileProjectUuid, file.uuid);
     });
 } else {
     it('Skipping integration tests', () => { });
