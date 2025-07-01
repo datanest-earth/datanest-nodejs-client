d6fa7151586e9b05afec49deefd550cf823b4b15
BrandonDR <brandon@derose.dev>
2025-07-02 10:49:50 +1200

    chore: added PROD env variable to limit concurrency

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitest/config'
 
+const isProduction = process.env.PROD === '1' || process.env.PROD === 'true';
+
 export default defineConfig({
     test: {
         hookTimeout: 30_000,
         testTimeout: 90_000,
         setupFiles: ['./tests/vitest-before-all.ts'],
+        ...(isProduction ? {
+            maxConcurrency: 1,
+            maxWorkers: 1,
+            minWorkers: 1,
+        } : {}),
     },
 })
